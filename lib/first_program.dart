4dffb8ff209f7dc5e6f4b2b4134308a912a0cf46
Chatmongcol <cdadwngs@gmail.com>
2023-07-06 15:26:26 +0700

    first commit

---
+++ lib/first_program.dart
@@ -0,0 +1,19 @@
+//Sub routine/function/method
+import 'dart:io';
+
+void firstProgram(){
+  var name = "Mark Zuckaberg";
+  var age = 45;
+  print(name);//call variable is name
+  print(age);//call variable is age
+  print("Hello World! I am $name, I am Age $age year old");
+}
+
+void secondProgram() {
+  int coffee;
+  print("Enter coffee count");
+  coffee = int.parse(stdin.readLineSync()!);
+  int price = 70;
+  int total = price * coffee;
+  print("ซื้อกาแฟ $coffee แก้ว ราคารวม $price * $coffee = $total ฿");
+}
